e4692aabd0b168e74d1587abe15155d5e0ff78d4
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2020-10-10 17:00:56 +0100

    test fix

--- google-geocode/src/test/java/br/com/esign/google/geocode/GoogleGeocodeTest.java
+++ google-geocode/src/test/java/br/com/esign/google/geocode/GoogleGeocodeTest.java
@@ -23,7 +23,7 @@ public class GoogleGeocodeTest {
 
 	@Test
 	public void testGetResponseObject2() throws IOException {
-		final String address = "R. 7 de Abril, 346 - Centro Histórico de São Paulo, São Paulo - SP, 01044-000, Brasil";
+		final String address = "R. Sete de Abril, 346 - Centro Histórico de São Paulo, São Paulo - SP, 01044-000, Brasil";
 		GoogleGeocode googleGeocode = new GoogleGeocode(apiKey, address);
 		googleGeocode.setLanguage(GoogleGeocodeLanguage.PORTUGUESE_BRAZIL);
 		GeocodeResponse geocodeResponse = googleGeocode.getResponseObject();
